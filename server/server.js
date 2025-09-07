d3a696c2bc1ffd317695075ddda85e329a91b7bb
Satyam Pal <151130006+Pal-Satyam@users.noreply.github.com>
2025-09-08 00:28:02 +0530

    Comment out server listen and export app
    Comment out the server listen function and export the app.

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use("/api/bookings", bookingRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+export default app;
