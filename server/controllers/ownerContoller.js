c268422a8c45760055dbfdd67c37de4bef0546ec
Satyam Pal <151130006+Pal-Satyam@users.noreply.github.com>
2025-09-08 14:05:16 +0530

    Fix import case for User model in ownerController

--- server/controllers/ownerContoller.js
+++ server/controllers/ownerContoller.js
@@ -1,5 +1,5 @@
 import Car from "../models/Car.js";
-import User from "../models/user.js";
+import User from "../models/User.js";
 import fs from "fs";
 import imagekit from "../configs/imageKit.js";
 import Booking from "../models/Booking.js";
